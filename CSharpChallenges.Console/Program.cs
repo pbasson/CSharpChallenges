880c4626d0b14d23248072d61930fa563aa007d1
preetpal basson <preet.basson@gmail.com>
2024-12-16 13:21:49 +0000

    v0.01: Initial Update

---
+++ CSharpChallenges.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using CSharpChallenges.CodeChallenges;
+
+namespace CSharpChallenges
+{
+    public static class Program
+    {
+
+        public static void Main(string[] str)
+        {
+            Console.WriteLine(new StringCode().AllPrefixes(CodeVariable.PrefixLength, CodeVariable.Words));
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ CSharpChallenges.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CSharpChallenges.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
