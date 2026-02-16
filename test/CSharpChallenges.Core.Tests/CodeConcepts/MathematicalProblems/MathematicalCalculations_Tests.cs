6892494edc72d878a083538149f71b300cbd0c02
preetpal basson <preet.basson@gmail.com>
2026-02-16 23:49:17 +0000

    0.34: Update

--- test/CSharpChallenges.Core.Tests/CodeConcepts/MathematicalProblems/MathematicalCalculations_Tests.cs
+++ test/CSharpChallenges.Core.Tests/CodeConcepts/MathematicalProblems/MathematicalCalculations_Tests.cs
@@ -49,4 +49,38 @@ public class MathematicalCalculations_Tests
         Assert.Equal(expected, result);
     }
 
+    [Theory]
+    [InlineData(6, 3, 2)]
+    [InlineData(-8, 4, -2)]
+    [InlineData(5, 2, 2.5)]
+    public void Divide_ShouldReturnExpectedValue(int a, int b, double expected)
+    {
+        // Arrange && Act
+        var result = _mathCalc.Divide(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Divide_ShouldReturnFalse_WhenDividingByZero()
+    {
+        // Arrange && Act && Assert
+        Assert.Throws<DivideByZeroException>(() => _mathCalc.Divide(5, 0));
+    }
+
+
+    [Theory]
+    [InlineData(5, 3, 2)]
+    [InlineData(-8, 4, 0)]
+    [InlineData(5, 2, 1)]
+    public void Modulus_ShouldReturnExpectedValue(int a, int b, int expected)
+    {
+        // Arrange && Act
+        var result = _mathCalc.Modulus(a, b);
+
+        // Assert
+        Assert.Equal(expected, result);     
+    }
+
 }
